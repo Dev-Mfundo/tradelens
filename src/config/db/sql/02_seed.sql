add79842230e29fcec82d23585b4a8b17fcec4c6
Aaron Jiane <devmfundo@gmail.com>
2025-05-21 16:47:15 +0200

    init and db connected, server tested

---
+++ src/config/db/sql/02_seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(first_name, last_name, email, phone_number)
+VALUES('Thando', 'Mokoena', 'thando@example.com', '+27601234567');
---
+++ src/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(morgan('dev'))
+
+app.get('/api/health', (req,res)=>{
+	res.status(200).json({
+		status: 'ok',
+		message: "testing api route and server"
+	})
+})
+
+app.listen(PORT,()=>console.log(`Server listening on: http://localhost:${PORT}`))
